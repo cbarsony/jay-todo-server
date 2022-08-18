8d1da2edf4f216b5eef93839da881b9af6931abf
cbarsony <csaba.barsony@gmail.com>
2022-08-18 13:52:56 +0200

    Add MySql

---
+++ db.js
@@ -0,0 +1,59 @@
+const mysql = require('mysql')
+require('dotenv').config()
+
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+con.connect(function(err) {
+    if (err) throw err
+    console.log("Connected to MySQL")
+})
+
+const todos = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            con.query('SELECT * FROM todos', (err, result) => {
+                if(err) reject(err)
+                resolve(result)
+            })
+        })
+    },
+    getById: todoId => {
+        return new Promise((resolve, reject) => {
+            con.query('SELECT * FROM todos WHERE id = ?', [todoId], (err, result) => {
+                if(err) reject(err)
+                resolve(result)
+            })
+        })
+    },
+    create: todo => {
+        return new Promise((resolve, reject) => {
+            con.query('INSERT INTO todos (text, is_completed) VALUES (?, ?)', [todo.text, todo.is_completed], (err, result) => {
+                if(err) reject(err)
+                resolve(result)
+            })
+        })
+    },
+    update: todo => {
+        return new Promise((resolve, reject) => {
+            con.query('UPDATE todos SET text = ?, is_completed = ? WHERE id = ?', [todo.text, todo.is_completed, todo.id], (err, result) => {
+                if(err) reject(err)
+                resolve()
+            })
+        })
+    },
+    delete: todoId => {
+        return new Promise((resolve, reject) => {
+            con.query('DELETE from todos WHERE id = ?', [todoId], (err, result) => {
+                if(err) reject(err)
+                resolve(result)
+            })
+        })
+    }
+}
+
+module.exports = {todos}
--- index.js
+++ index.js
@@ -1,72 +1,53 @@
 const express = require('express')
+const db = require('./db')
 
 const PORT = process.env.PORT || 3001
 const app = express()
-const nextTodoId = todos => todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1
-const db = {
-    todos: [
-        {
-          id: 1,
-          text: 'Take sunbath',
-          isCompleted: false,
-        },
-        {
-          id: 2,
-          text: 'Go for a walk',
-          isCompleted: true,
-        },
-    ],
-}
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    console.log(`\nRequest: ${req.method} ${req.url}`)
+    console.log(`Request body: ${JSON.stringify(req.body)}`)
+    next()
+})
+
 app.get("/todos", (req, res) => {
-    console.log(`GET /todos ${JSON.stringify(db.todos)}`)
-    res.json(db.todos)
+    db.todos.getAll()
+        .then(todos => res.json(todos))
+        .catch(error => res.sendStatus(500))
 })
 
 app.get('/todos/:id', (req, res) => {
     const todoId = Number(req.params.id)
-    const todo = db.todos.find(todo => todo.id === todoId)
 
-    console.log(`GET /todos/${todoId}`)
-    res.json(todo)
+    db.todos.getById(todoId)
+        .then(todo => res.json(todo))
+        .catch(error => res.sendStatus(500))
 })
 
 app.post('/todos', (req, res) => {
-    const newTodo = {
-        id: nextTodoId(db.todos),
-        text: req.body.text,
-        isCompleted: req.body.isCompleted,
-    }
-
-    db.todos = [...db.todos, newTodo]
-
-    console.log(`POST /todos ${JSON.stringify(newTodo)}`)
-    res.sendStatus(200)
+    db.todos.create({text: req.body.text, is_completed: req.body.isCompleted})
+        .then(result => res.json(result.insertId))
+        .catch(error => res.sendStatus(500))
 })
 
 app.put('/todos/:id', (req, res) => {
-    const todoId = Number(req.params.id)
-    const updatedTodo = {
-        id: todoId,
+    db.todos.update({
+        id: Number(req.body.id),
         text: req.body.text,
-        isCompleted: req.body.isCompleted,
-    }
-
-    db.todos = db.todos.map(todo => todo.id === todoId ? updatedTodo : todo)
-
-    console.log(`PUT /todos/${todoId} ${JSON.stringify(updatedTodo)}`)
-    res.sendStatus(200)
+        is_completed: req.body.isCompleted,
+    })
+        .then(() => res.sendStatus(200))
+        .catch(error => res.sendStatus(500))
 })
 
 app.delete('/todos/:id', (req, res) => {
     const todoId = Number(req.params.id)
 
-    db.todos = db.todos.filter(todo => todo.id !== todoId)
-
-    console.log(`DELETE /todos/${todoId}`)
-    res.sendStatus(200)
+    db.todos.delete(todoId)
+        .then(() => res.sendStatus(200))
+        .catch(error => res.sendStatus(500))
 })
 
 app.listen(PORT, () => {
